7960819b786d4450e13a69eb7f39870464df8422
cfigueirasvalverde <cfigueirasvalverde@nautilus20.danielcastelao.org>
2018-11-21 11:32:41 +0100

    Boletin11

---
+++ Boletin11/src/boletin11/Boletin11.java
@@ -0,0 +1,16 @@
+package boletin11;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Tiempo tardar = new Tiempo();
+    }
+    
+}
